fb8d810ddd4d4832bfc5e4e8daa63ed97495e2c1
Asdmir786 <asmir.alams.com@gmail.com>
2024-12-26 21:33:59 +0500

    Add functions to display number range and validate user input in Number Guessing game

--- Projects/10. Number Guessing game.py
+++ Projects/10. Number Guessing game.py
@@ -4,3 +4,18 @@ lowest_number = 0
 highest_number = 100
 
 target_number  = random.randint(lowest_number,highest_number)
+
+def tell_numbers():
+    print(f"The Lowest Number: {lowest_number}.\nThe Highest Number: {highest_number}.")
+
+def ask_user_input():
+    while True:
+        try:
+            user_input = int(input("Enter your number: "))
+            if lowest_number <= user_input >= highest_number:
+                return user_input
+            else:
+                print(f"Enter a number less than {highest_number} or graeter than {lowest_number}. ")
+        except ValueError:
+            print("Enter a number.")
+            
