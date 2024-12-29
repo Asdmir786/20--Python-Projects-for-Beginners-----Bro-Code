a989d307710767816a5954b0dc32073b6a1831d5
Asdmir786 <asmir.alams.com@gmail.com>
2024-12-29 23:35:07 +0500

    Enhance dice display functionality to return rolled values and calculate total

--- Projects/12. Dice Roller program.py
+++ Projects/12. Dice Roller program.py
@@ -46,22 +46,32 @@ dice_ascii = {
 }
 
 def display_dice(total_number_of_dice=1):
+    list_of_dice = []
     count = 0
     while count < total_number_of_dice:
         # Check if there are at least two dice left to roll
         if total_number_of_dice - count != 1 and not(total_number_of_dice - count <= 0):  
             random_number = random.randint(1, 6)
             random_number_2 = random.randint(1, 6)
+            list_of_dice.append(random_number)
+            list_of_dice.append(random_number_2)
             # Print two dice side by side
             for j in range(len(dice_ascii[1])):
                 print(f"{dice_ascii[random_number][j]}\t {dice_ascii[random_number_2][j]}")
             count += 2
         else:
             random_number_3 = random.randint(1, 6)
+            list_of_dice.append(random_number_3)
             # Print one die
             for j in range(len(dice_ascii[1])):
                 print(f"{dice_ascii[random_number_3][j]}")
             count += 1
+    print(list_of_dice)
+    return list_of_dice
+
+def add_all_dices(list_dice=list):
+    sum_of_dices = sum(list_dice)
+    print(f"Total: {sum_of_dices}")
 
 def main():
     while True:
@@ -72,7 +82,10 @@ def main():
         except ValueError:
             print("Enter a Number.")
     
-    display_dice(times_to_roll)
+    list_of_dice = display_dice(times_to_roll)
+    add_all_dices(list_of_dice)
+
+    
 
 if __name__ == "__main__":
     main()
