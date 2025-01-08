020b7f08e6641e35451afe4489738fb83b44ad12
Asdmir786 <asmir.alams.com@gmail.com>
2025-01-08 21:07:25 +0500

    Add ignore_letters list and update Caesar_Cypher to filter out ignored characters

--- Projects/13. Encryption Program.py
+++ Projects/13. Encryption Program.py
@@ -5,8 +5,13 @@ letters = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
 ]
 
+ignore_letters = [
+    " ", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")" , "[]", "{}",":",
+    ";", "'", "\"", " |", "\\", ",", "<>", "/", "?", "_", "-", "=","+"
+    ]
+
 # Caesar cipher equivalent with shift by 5
-caesar_letters = [
+caesar_letters_5 = [
     'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D',
     'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
     '5', '6', '7', '8', '9', '0', '1', '2', '3', '4'
@@ -38,11 +43,15 @@ def ask_user_for_text(user_input):
             return user_string
 
 def Caesar_Cypher(user_string):
+    match_index = []
     user_string = list(user_string)
     # print(user_string)
-    for i in range(len(user_string)):
-        if user_string[i] in letters:
-            pass
+    for I in user_string:
+        if I in ignore_letters:
+            continue
+        match_index.append(letters.index(I))
+    print(match_index)
+
 user_string = "Hamood Wal hamoodi"
 
 Caesar_Cypher(user_string)
