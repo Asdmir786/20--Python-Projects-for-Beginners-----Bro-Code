e428bbaaccc1367c6056049c3b510fcd6689c991
Asdmir786 <asmir.alams.com@gmail.com>
2025-01-19 00:37:42 +0500

    Import hangman game words and print list count for debugging

---
+++ Projects/17. Hangman game.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append("C:\\Users\\asmir\\Documents\\20--Python-Projects-for-Beginners-----Bro-Code")
+from ProjectsResources import hangman_game_words as h
+
+print(len(h.words))
