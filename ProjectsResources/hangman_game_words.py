d24e115aa3d0270a5a5565c848031523b967836a
Asdmir786 <asmir.alams.com@gmail.com>
2025-01-19 00:37:09 +0500

    Add word list for Hangman game

---
+++ ProjectsResources/hangman_game_words.py
@@ -0,0 +1,12 @@
+words = [
+    "photosynthesis", "gravity", "eclipse", "molecule", "calculus",
+    "prophet", "hadith", "qibla", "hijra", "zakat",
+    "continent", "equator", "latitude", "volcano", "savanna",
+    "renaissance", "dynasty", "constitution", "revolution", "empire",
+    "algorithm", "geometry", "integer", "derivative", "vector",
+    "architecture", "blueprint", "scaffolding", "foundation", "bricklayer",
+    "ecosystem", "neutron", "oxidation", "cellular", "magnetism",
+    "quran", "sawm", "hajj", "imam", "khutbah",
+    "plateau", "delta", "archipelago", "fjord", "peninsula",
+    "archaeology", "crusade", "colonization", "artifact", "manuscript"
+]
