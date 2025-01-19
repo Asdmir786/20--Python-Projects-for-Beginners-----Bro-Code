89cb92e76f7714c901c0c6278d6c1fefc375bb57
Asdmir786 <asmir.alams.com@gmail.com>
2025-01-19 17:12:02 +0500

    Add hangman game states and word selection function

--- Projects/17. Hangman game.py
+++ Projects/17. Hangman game.py
@@ -1,5 +1,76 @@
 import sys
 sys.path.append("C:\\Users\\asmir\\Documents\\20--Python-Projects-for-Beginners-----Bro-Code")
 from ProjectsResources import hangman_game_words as h
+import random
 
-print(len(h.words))
+hangman_states = [
+    """
+       _______
+      |/      |
+      |       
+      |       
+      |        
+      |       
+     _|___    
+    """,
+    """
+       _______
+      |/      |
+      |      ( )
+      |       
+      |        
+      |       
+     _|___    
+    """,
+    """
+       _______
+      |/      |
+      |      ( )
+      |       |
+      |        
+      |       
+     _|___    
+    """,
+    """
+       _______
+      |/      |
+      |      ( )
+      |      /|
+      |        
+      |       
+     _|___    
+    """,
+    """
+       _______
+      |/      |
+      |      ( )
+      |      /|\\
+      |        
+      |       
+     _|___    
+    """,
+    """
+       _______
+      |/      |
+      |      ( )
+      |      /|\\
+      |      / 
+      |       
+     _|___    
+    """,
+    """
+       _______
+      |/      |
+      |      ( )
+      |      /|\\
+      |      / \\
+      |       
+     _|___    
+    """
+]
+
+def get_word():
+    word = random.choice(h.words)
+    return word
+
+print(get_word())
