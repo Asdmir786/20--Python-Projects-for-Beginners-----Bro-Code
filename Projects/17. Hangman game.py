9689c57dfff1f4fb151c765d25c8fcc81b2d0e24
Asdmir786 <asmir.alams.com@gmail.com>
2025-01-19 18:40:48 +0500

    Add functions to display hangman state, clear screen, and provide word hints

--- Projects/17. Hangman game.py
+++ Projects/17. Hangman game.py
@@ -2,6 +2,8 @@ import sys
 sys.path.append("C:\\Users\\asmir\\Documents\\20--Python-Projects-for-Beginners-----Bro-Code")
 from ProjectsResources import hangman_game_words as h
 import random
+import os
+import time
 
 hangman_states = [
     """
@@ -73,4 +75,20 @@ def get_word():
     word = random.choice(h.words)
     return word
 
-print(get_word())
+def display_state(number=0):
+    print(hangman_states[number])
+
+def clear_screen(userInput_or_timeInput="",seconds=0,prompt="Press enter or anything to clear screen.\n: "):
+    userInput_or_timeInput =  userInput_or_timeInput.lower().strip()
+    
+    if userInput_or_timeInput == "ui":
+        input(prompt)
+        os.system("cls")
+    else:
+        time.sleep(seconds)
+        os.system("cls")
+    
+def display_hint(word):
+    hint = "_ "*len(word)
+    print(hint)
+
