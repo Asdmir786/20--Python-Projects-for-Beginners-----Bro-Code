0ecf9e1a35967bc3b69e49d267f5ff969d6a3676
Asdmir786 <asmir.alams.com@gmail.com>
2025-01-20 09:58:40 +0500

    Implement user input handling and game loop for guessing in hangman

--- Projects/17. Hangman game.py
+++ Projects/17. Hangman game.py
@@ -91,6 +91,38 @@ def clear_screen(userInput_or_timeInput="",seconds=0,prompt="Press enter or anyt
 def display_hint(word):
     hint = "_ "*len(word)
     print(hint)
+    return hint
+
+def ask_userWord():
+    while True:
+        user_input = input("Guess the word: ").lower().strip()
+        if len(user_input) > 1:
+            print("Write 1 word at a time.")
+        else:
+            return user_input
 
 def main():
     main_word = get_word()
+    wrong_guesses = 0
+    total_guesses = 0
+    
+    while wrong_guesses != len(hangman_states):
+        display_state(wrong_guesses)
+        hint = display_hint(main_word)
+        word_input = ask_userWord()
+
+        for i in range(len(main_word)):
+            if word_input == main_word[i]:
+                print(f"{word_input} is indeed in the {main_word}")    
+                total_guesses += 1
+                break
+            else:
+                print("Nope")
+                total_guesses += 1
+                wrong_guesses += 1
+                break
+    print("Too many guesses")
+    
+if __name__ == "__main__":
+    main()
+    
