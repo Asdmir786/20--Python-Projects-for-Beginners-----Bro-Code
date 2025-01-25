2668cd605442c50df397fcfe8e582b128e720112
Asdmir786 <asmir.alams.com@gmail.com>
2025-01-26 01:46:31 +0500

    Refactor alarm clock script to introduce user input for setting time

--- Projects/18. Alarm Clock.py
+++ Projects/18. Alarm Clock.py
@@ -1,7 +1,11 @@
 import time
 import datetime as d
 
-while True:
-    current_time = d.datetime.now().strftime("%H:%M:%S")
-    print(current_time)
-    time.sleep(1)
+def get_date():
+    while True:
+        user_input = input("Enter the time to BLYAAAT your OS(hh:mm:ss): ")
+
+# while True:
+#     current_time = d.datetime.now().strftime("%H:%M:%S")
+#     print(current_time)
+#     time.sleep(1)
