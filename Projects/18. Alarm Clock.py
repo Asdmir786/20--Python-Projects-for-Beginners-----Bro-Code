9d8914302f236c8669356eee1b2acf37723fa46e
Asdmir786 <asmir.alams.com@gmail.com>
2025-01-26 21:47:28 +0500

    wip fr fr

--- Projects/18. Alarm Clock.py
+++ Projects/18. Alarm Clock.py
@@ -4,6 +4,11 @@ import datetime as d
 def get_date():
     while True:
         user_input = input("Enter the time to BLYAAAT your OS(hh:mm:ss): ")
+        parts = user_input.split(":")
+        if len(parts) != 3:
+            print("Enter with this pattern hh:mm:ss: ")
+        else: break
+    print(parts)
 
 # while True:
 #     current_time = d.datetime.now().strftime("%H:%M:%S")
