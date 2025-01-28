ddc1d2893b57738e78af77153e9eddbfb80baaa6
Asdmir786 <asmir.alams.com@gmail.com>
2025-01-29 01:38:33 +0500

    testing purposes

---
+++ ProjectsResources/test.py
@@ -0,0 +1,13 @@
+# import datetime as d 
+
+# days = 1
+# hours = 12
+# minutes = 42
+# seconds = 58
+
+# current_time = d.datetime.now().strftime("%d %H:%M:%S")
+# user_date = d.datetime.now().replace(day=days,hour=hours,minute=minutes,second=seconds,microsecond=0).strftime("%d %H:%M:%S")
+
+# print(current_time)
+# print("="*12)
+# print(user_date)
