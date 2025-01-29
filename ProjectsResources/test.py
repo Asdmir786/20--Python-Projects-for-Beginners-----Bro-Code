3776a63258231d3d25a9303580cf6f7b30cb3dfa
Asdmir786 <asmir.alams.com@gmail.com>
2025-01-30 00:25:55 +0500

    Add set_alarm function to schedule alarms with user-defined time

--- ProjectsResources/test.py
+++ ProjectsResources/test.py
@@ -11,3 +11,28 @@
 # print(current_time)
 # print("="*12)
 # print(user_date)
+import datetime as d
+import time
+from pygame import mixer
+from keyboard import wait
+import pathlib
+
+path = pathlib.Path("ProjectsResources/Audios")
+audios = [audio for audio in path.iterdir()]
+
+def set_alarm(user_date="", audio_number = 0):
+    parts = user_date.split(":")
+    hourss,minutess,secondss = map(int,parts)
+    time_to_blyat = d.time(hourss,minutess,secondss)
+
+    while True:
+        current_time = d.datetime.now().strftime("%H:%M:%S")
+        if current_time == str(time_to_blyat):
+            print(f"{current_time} = {time_to_blyat}")
+            break
+        else:
+            print(current_time)
+            print(type(current_time))
+            time.sleep(1)
+
+set_alarm("23:11:00")
